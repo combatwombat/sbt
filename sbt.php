e1de3b88ae2688b43b0b301ed418d2ddce8ff006
Robert Gerlach <robert.gerlach@gmail.com>
2018-05-28 00:21:50 +0200

    initial commit

---
+++ sbt.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Simple Bookmark Tool
+ * Robert Gerlach 2018
+ */
+
+$config = array(
+    'db' => array(
+        'name'      => 'sbt',
+        'user'      => 'root',
+        'password'  => 'root',
+        'host'      => '127.0.0.1',
+        'port'      => '3306'
+    ),
+    'access' => array(
+        'user'      => 'sbt',
+        'password'  => 'sbt'
+    )
+);
+
