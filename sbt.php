4ff454d621e3355377e300820085835417a751ca
Robert Gerlach <robert.gerlach@gmail.com>
2018-05-28 23:25:46 +0200

    break text on mobile

--- sbt.php
+++ sbt.php
@@ -278,6 +278,13 @@ function routeIndex() {
                 border-bottom: 1px solid #ccc;
             }
 
+            @media (max-width: 400px) {
+                .items h2,
+                .items .description,
+                .items .url {
+                    word-break: break-all;
+                }
+            }
             @media (max-width: 340px) {
                 h1 {
                     font-size: 1.9em;
